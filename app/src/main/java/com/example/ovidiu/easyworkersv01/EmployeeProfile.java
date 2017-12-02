9465b85562e3ef7276ed847c914c2937e6ee1064
ovidiumuntean <ovidiumuntean2006@gmail.com>
2017-12-02 22:01:39 +0000

    Second Commit WIth login/Register profile, I have to work on profile and login to connect to the database, For register I have  to insert into the database( I have to do some fixes).

---
+++ app/src/main/java/com/example/ovidiu/easyworkersv01/EmployeeProfile.java
@@ -0,0 +1,13 @@
+package com.example.ovidiu.easyworkersv01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployeeProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_profile);
+    }
+}
