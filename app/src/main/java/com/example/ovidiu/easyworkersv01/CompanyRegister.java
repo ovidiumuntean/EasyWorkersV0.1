d652d8428a60feeb2f71fb0e3ed22809d6ec6c3e
anamali <caoimhe2>
2017-12-05 19:04:56 +0000

    commit reg

---
+++ app/src/main/java/com/example/ovidiu/easyworkersv01/CompanyRegister.java
@@ -0,0 +1,379 @@
+package com.example.ovidiu.easyworkersv01;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ovidiu.easyworkersv01.Entity.Company;
+import com.example.ovidiu.easyworkersv01.Entity.Employee;
+import com.example.ovidiu.easyworkersv01.Util.AlertDialogManager;
+import com.example.ovidiu.easyworkersv01.Util.DatabaseManager;
+import com.example.ovidiu.easyworkersv01.Util.EmailValidator;
+import com.example.ovidiu.easyworkersv01.Util.SessionManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class CompanyRegister extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+
+    /**
+     * Id to identity READ_CONTACTS permission request.
+     */
+    private static final int REQUEST_READ_CONTACTS = 0;
+    SessionManager session;
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world"
+    };
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginTask mAuthTask = null;
+
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private EditText mConfPasswordView;
+    private EditText mNameView;
+    private EditText mRegNumView;
+    private EditText mPhoneNoView;
+    private EditText mAddress;
+    private View mProgressView;
+    private View mLoginFormView;
+    private DatabaseManager myDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_register);
+        session = new SessionManager(getApplicationContext());
+        // Set up the login form.
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mNameView = (EditText) findViewById(R.id.companyName);
+        mPhoneNoView = (EditText) findViewById(R.id.phone);
+        mAddress = (EditText) findViewById(R.id.Address);
+        mRegNumView = (EditText) findViewById(R.id.registerationNumber);
+        myDb = new DatabaseManager(this, null, null, 1);
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mConfPasswordView = (EditText) findViewById(R.id.password_confirmedRegE);
+
+        mConfPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptRegister();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptRegister();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+    }
+
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptRegister() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+        mNameView.setError(null);
+        mRegNumView.setError(null);
+        mPhoneNoView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        String confirmPassword = mConfPasswordView.getText().toString();
+        String Name = mNameView.getText().toString();
+        String regNo = mRegNumView.getText().toString();
+        String phoneNo = mPhoneNoView.getText().toString();
+        String Address = mAddress.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid confirmed password, if the user entered one.
+        if (!TextUtils.isEmpty(confirmPassword) && !isPasswordValid(confirmPassword)) {
+            mConfPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mConfPasswordView;
+            cancel = true;
+        } else {
+            if (!confirmPassword.equals(password)) {
+                mConfPasswordView.setError(getString(R.string.error_invalid_confirmed_password));
+                focusView = mConfPasswordView;
+                cancel = true;
+            }
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(Name)) {
+            mNameView.setError(getString(R.string.error_field_required));
+            focusView = mNameView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(regNo)) {
+            mRegNumView.setError(getString(R.string.error_field_required));
+            focusView = mRegNumView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(phoneNo)) {
+            mPhoneNoView.setError(getString(R.string.error_field_required));
+            focusView = mPhoneNoView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt register and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            Company company = new Company(0, regNo, Name, phoneNo, Address, email, password);
+            mAuthTask = new UserLoginTask(company);
+            mAuthTask.doInBackground();
+            mAuthTask.execute((Void) null);
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        EmailValidator emailValidator = new EmailValidator();
+        return emailValidator.validate(email);
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(this,
+                // Retrieve data rows for the device user's 'profile' contact.
+                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
+                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), CompanyRegister.ProfileQuery.PROJECTION,
+
+                // Select only email addresses.
+                ContactsContract.Contacts.Data.MIMETYPE +
+                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
+                .CONTENT_ITEM_TYPE},
+
+                // Show primary email addresses first. Note that there won't be
+                // a primary email address if the user hasn't specified one.
+                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        List<String> emails = new ArrayList<>();
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            emails.add(cursor.getString(CompanyRegister.ProfileQuery.ADDRESS));
+            cursor.moveToNext();
+        }
+
+        addEmailsToAutoComplete(emails);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+
+    }
+
+    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
+        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(CompanyRegister.this,
+                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
+
+        mEmailView.setAdapter(adapter);
+    }
+
+
+    private interface ProfileQuery {
+        String[] PROJECTION = {
+                ContactsContract.CommonDataKinds.Email.ADDRESS,
+                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
+        };
+
+        int ADDRESS = 0;
+        int IS_PRIMARY = 1;
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private Company company;
+
+        UserLoginTask(Company company) {
+            this.company = company;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+            Company comp;
+            try {
+                // Simulate network access.
+                comp = myDb.searchCompanyByEmail(company.getEmail());
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            if(comp == null){
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            showProgress(false);
+            AlertDialogManager alert = new AlertDialogManager();
+            if (success) {
+                if(myDb.addCompany(company)) {
+                    if (session.isLoggedIn()) {
+                        session.logoutUser();
+                    }
+                    session.createLoginSession(company.getName(), company.getEmail());
+                    Toast.makeText(CompanyRegister.this, "User " + company.getName() + " successfully logged in!", Toast.LENGTH_SHORT).show();
+                    alert.showAlertDialog(CompanyRegister.this, "Registration successfully..", "User " + company.getName() + " successfully registered!", false);
+                    Intent compProfIntent = new Intent(CompanyRegister.this, CompanyProfile.class);
+                    startActivity(compProfIntent);
+                } else {
+
+                    alert.showAlertDialog(CompanyRegister.this, "Registration failed..", "Invalid Data!", false);
+                }
+
+            } else {
+                //Toast.makeText(this, "Please Try Again!", Toast.LENGTH_SHORT).show();
+                alert.showAlertDialog(CompanyRegister.this, "Registration failed..", "You are already registered! Please Login!", false);
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
+    }
+}
+
+
+
