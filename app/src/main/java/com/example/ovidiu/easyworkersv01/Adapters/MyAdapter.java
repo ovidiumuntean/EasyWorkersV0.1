771c591af3eab8f3a39418fe497f9c4253441d41
anamali <caoimhe2>
2017-12-11 15:04:37 +0000

    commit

---
+++ app/src/main/java/com/example/ovidiu/easyworkersv01/Adapters/MyAdapter.java
@@ -0,0 +1,8 @@
+package com.example.ovidiu.easyworkersv01.Adapters;
+
+/**
+ * Created by anamali on 11/12/2017.
+ */
+
+public class MyAdapter {
+}
--- app/src/main/java/com/example/ovidiu/easyworkersv01/Tables/JobTable.java
+++ app/src/main/java/com/example/ovidiu/easyworkersv01/Tables/JobTable.java
@@ -2,16 +2,16 @@ package com.example.ovidiu.easyworkersv01.Tables;
 
 import android.content.ContentValues;
 
-import com.example.ovidiu.easyworkersv01.Entity.Job;
+import com.example.ovidiu.easyworkersv01.Entity.JobEntity;
 import com.example.ovidiu.easyworkersv01.Util.UsefullyFunctions;
 
 /**
  * Created by ovidiu on 01/12/2017.
  */
 
-public class JobTable {
+public class Job {
 
-    private static final String TABLE_NAME = "Job";
+    private static final String TABLE_NAME = "JobEntity";
     private static final String COL_ID = "ID";
     private static final String COL_TITLE = "TITLE";
     private static final String COL_DESCRIPTION = "DESCRIPTION";
@@ -22,7 +22,7 @@ public class JobTable {
     private static final String COL_COMPANY_ID = "COMPANY_ID";
 
 
-    public JobTable() {
+    public Job() {
     }
 
     public String createTableQuery() {
@@ -38,15 +38,15 @@ public class JobTable {
         return createJobTable;
     }
 
-    public ContentValues createValues(Job job) {
+    public ContentValues createValues(JobEntity jobEntity) {
         UsefullyFunctions util = new UsefullyFunctions();
         ContentValues values = new ContentValues();
-        values.put(COL_TITLE, job.getTitle());
-        values.put(COL_DESCRIPTION, job.getDescription());
-        values.put(COL_TYPE, job.getType());
-        values.put(COL_EXPERIENCE, job.getExperience());
-        values.put(COL_CATEGORY, job.getCategory());
-        values.put(COL_COMPANY_ID, job.getCompany().getId());
+        values.put(COL_TITLE, jobEntity.getTitle());
+        values.put(COL_DESCRIPTION, jobEntity.getDescription());
+        values.put(COL_TYPE, jobEntity.getType());
+        values.put(COL_EXPERIENCE, jobEntity.getExperience());
+        values.put(COL_CATEGORY, jobEntity.getCategory());
+        values.put(COL_COMPANY_ID, jobEntity.getCompany().getId());
         return values;
     }
 
